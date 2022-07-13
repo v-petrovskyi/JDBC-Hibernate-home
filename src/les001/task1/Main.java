1c4daa366a106012d3585d18909378c0dc27efb3
Volodymyr <bugsbunny1163@gmail.com>
2022-07-13 19:45:11 +0300

    les001 done

---
+++ src/les001/task1/Main.java
@@ -0,0 +1,53 @@
+package les001.task1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        final String URL = "jdbc:mysql://localhost:3306/myjoinsdb1";
+        final String LOGIN = "root";
+        final String PASS = "root1";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        File file = new File("src/les001/task1/sql query.txt");
+        Scanner scanner = new Scanner(file);
+        Connection connection = null;
+        Statement statement = null;
+
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            try {
+                connection = DriverManager.getConnection(URL, LOGIN, PASS);
+                statement = connection.createStatement();
+                statement.execute(line);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                if (statement != null) {
+                    try {
+                        statement.close();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
+}
--- src/les001/task6/Main.java
+++ src/les001/task6/Main.java
@@ -1,4 +1,50 @@
 package les001.task6;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
+    public static void main(String[] args) {
+        final String URL = "jdbc:mysql://localhost:3306/myjoinsdb1";
+        final String LOGIN = "root";
+        final String PASS = "root1";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            connection = DriverManager.getConnection(URL, LOGIN, PASS);
+            statement = connection.createStatement();
+            statement.execute("CREATE TABLE test_table (id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, user_name VARCHAR(30), password VARCHAR(30))");
+            statement.executeUpdate("INSERT INTO test_table(user_name, password) VALUES ('Andrii', 'qwerty123')");
+            statement.executeUpdate("INSERT INTO test_table(user_name, password) VALUES ('super_user', 'jkf:OUG40(&')");
+            statement.executeUpdate("INSERT INTO test_table(user_name, password) VALUES ('John', 'some-password')");
+            statement.executeUpdate("INSERT INTO test_table(user_name, password) VALUES ('Olga', '****')");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection!=null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (statement!=null){
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
